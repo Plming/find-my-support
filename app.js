e2c0c7f23d46927dde7ef00341d68464aaf20642
Kim Hyeonseok <kim010322@gmail.com>
2021-11-18 05:54:22 +0900

    change to serve static file

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
+"use strict";
+
 const express = require("express");
 const path = require("path");
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT ?? 3000;
 
 const htmlPath = path.resolve(__dirname, "html");
+
+app.use("/js", express.static("js"));
 
 app.get("/", (req, res) => {
   res.sendFile(path.resolve(htmlPath, "index.html"));
