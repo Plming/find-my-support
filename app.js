908da319890972f9e65a6a034342e2aa883a48f6
Kim Hyeonseok <kim010322@gmail.com>
2021-12-12 04:13:35 +0900

    redesign result page and remove legacy

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ const app = express();
 const port = process.env.PORT ?? 3000;
 const base = "https://api.odcloud.kr/api/gov24/v1/";
 const API_AUTH_KEY = process.env.API_AUTH_KEY;
-const htmlPath = path.resolve(__dirname, "views");
+
+app.use("/public", express.static(__dirname + "/public"));
 
 app.set("views", "views");
 app.set("view engine", "ejs");
@@ -24,36 +25,42 @@ app.get("/entry", (req, res) => {
 });
 
 app.get("/result", async (req, res) => {
-  const url = new URL("serviceList", base);
-  url.search = new URLSearchParams({
-    page: 1,
-    perPage: 10,
-    serviceKey: API_AUTH_KEY,
-  });
+  let result = [];
+  let currentPage = 0;
 
-  const data = await fetchFromApi(url.toString());
+  while (result.length < 10) {
+    const url = new URL("serviceList", base);
+    url.search = new URLSearchParams({
+      page: currentPage,
+      perPage: 10,
+      serviceKey: API_AUTH_KEY,
+    });
 
-  let listToShow = [];
-  for (let i = 0; i < data.length; ++i) {
-    if (!(await canServiced(req.query, data[i]["서비스ID"]))) {
-      continue;
-    }
-
-    // HACK: manipulate url
-    data[i]["상세조회URL"] = data[i]["상세조회URL"].replace(
-      "//gov.kr",
-      "//www.gov.kr"
-    );
-
-    listToShow.push(data[i]);
-
-    // TODO: in development
-    if (listToShow.length >= 4) {
+    let body = await fetchFromApi(url.toString());
+    console.log(currentPage + " called");
+    if (body.currentCount === 0) {
       break;
     }
+
+    let list = body.data;
+    for (let i = 0; i < list.length; ++i) {
+      if (!(await canServiced(req.query, list[i]["서비스ID"]))) {
+        continue;
+      }
+
+      // HACK: manipulate url
+      list[i]["상세조회URL"] = list[i]["상세조회URL"].replace(
+        "//gov.kr",
+        "//www.gov.kr"
+      );
+
+      result.push(list[i]);
+    }
+
+    ++currentPage;
   }
 
-  res.render("result", { serviceList: listToShow });
+  res.render("result", { serviceList: result });
 });
 
 app.listen(port, () => {
@@ -64,29 +71,29 @@ app.listen(port, () => {
 
 async function fetchFromApi(url) {
   const response = await fetch(url);
-  const jsonOut = await response.json();
+  const result = await response.json();
 
-  assert(response.status === 200, jsonOut.msg);
+  // assert(response.status === 200, jsonOut.msg);
 
-  return jsonOut.data;
+  return result;
 }
 
 async function canServiced(conditionQuery, serviceId) {
   const url = new URL("supportConditions", base);
   url.search = new URLSearchParams({
     "cond[SVC_ID::EQ]": serviceId,
+    page: 1,
+    perPage: 1,
     serviceKey: API_AUTH_KEY,
   });
 
-  const data = await fetchFromApi(url.toString());
-
-  const conditions = data[0];
-
+  const body = await fetchFromApi(url.toString());
   // TODO: there's a service list that no support conditions provided
-  if (conditions === undefined) {
+  if (body.currentCount === 0) {
     return true;
   }
 
+  const conditions = body.data[0];
   for (const [_, value] of Object.entries(conditionQuery)) {
     if (value === "") {
       continue;
